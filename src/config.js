26850b7b8283042cd020cb79feee29ecb4750113
prasad417 <417.prasad@gmail.com>
2021-07-28 14:08:09 -0500

    update redirect uri

--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@ export default {
   oidc: {
     clientId: CLIENT_ID,
     issuer: ISSUER,
-    redirectUri: 'http://localhost:3000/implicit/callback',
+    redirectUri: 'http://localhost:8080/login/callback',
     scopes: ['openid', 'profile'],
     pkce: true,
     disableHttpsCheck: OKTA_TESTING_DISABLEHTTPSCHECK,
